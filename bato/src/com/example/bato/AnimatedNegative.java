acc8bae49133d50145ccd0849ea3d246acb563e4
Sam Portnow <samson91787@gmail.com>
2013-03-16 18:08:07 -0400

    changed text size

--- bato/src/com/example/bato/AnimatedNegative.java
+++ bato/src/com/example/bato/AnimatedNegative.java
@@ -199,11 +199,11 @@ public class AnimatedNegative extends View
         	    	positive_paint.setColor(Color.parseColor("#FF4444"));
                	 	positive_paint.setShadowLayer(5, 2, 2, Color.YELLOW);
                 	positive_paint.setTypeface(Typeface.DEFAULT_BOLD);
-        	    	positive_paint.setTextSize(height/25);
+        	    	positive_paint.setTextSize(25);
                 	score.setAntiAlias(true);
                 	score.setTypeface(Typeface.DEFAULT_BOLD);
 
-                	score.setTextSize(30);
+                	score.setTextSize(height/25);
             	    score.setColor(Color.CYAN);
             	    score.setShadowLayer(1, 1, 1, Color.RED);
                 	score_background.setStyle(TextPaint.Style.FILL);
