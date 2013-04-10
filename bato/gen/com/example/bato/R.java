82fca4da52deb1041864d3bc0e4e1b9f63902340
Sam Portnow <samson91787@gmail.com>
2013-04-09 23:43:50 -0400

    current copy

--- bato/gen/com/example/bato/R.java
+++ bato/gen/com/example/bato/R.java
@@ -8,10 +8,6 @@
 package com.example.bato;
 
 public final class R {
-    public static final class array {
-        public static final int likert_enjoyment=0x7f060000;
-        public static final int likert_importance=0x7f060001;
-    }
     public static final class attr {
     }
     public static final class drawable {
@@ -19,137 +15,122 @@ public final class R {
         public static final int asteroid_explode2=0x7f020001;
         public static final int asteroid_explode3=0x7f020002;
         public static final int asteroid_explode4=0x7f020003;
-        public static final int cloud=0x7f020004;
-        public static final int dark_clouds=0x7f020005;
-        public static final int event=0x7f020006;
-        public static final int graycloud=0x7f020007;
-        public static final int ic_launcher=0x7f020008;
-        public static final int images=0x7f020009;
-        public static final int lightning=0x7f02000a;
-        public static final int red=0x7f02000b;
-        public static final int sad=0x7f02000c;
-        public static final int smiley=0x7f02000d;
-        public static final int sun=0x7f02000e;
-        public static final int thunder=0x7f02000f;
-        public static final int write=0x7f020010;
-        public static final int writer=0x7f020011;
+        public static final int bag=0x7f020004;
+        public static final int border_bottom_dashed=0x7f020005;
+        public static final int button=0x7f020006;
+        public static final int cannon=0x7f020007;
+        public static final int cloud=0x7f020008;
+        public static final int cloudsun=0x7f020009;
+        public static final int dark_clouds=0x7f02000a;
+        public static final int gradient=0x7f02000b;
+        public static final int graycloud=0x7f02000c;
+        public static final int green_bag=0x7f02000d;
+        public static final int ic_launcher=0x7f02000e;
+        public static final int question=0x7f02000f;
+        public static final int red=0x7f020010;
+        public static final int red_cannon=0x7f020011;
+        public static final int sad=0x7f020012;
+        public static final int smiley=0x7f020013;
+        public static final int sun=0x7f020014;
+        public static final int thunder=0x7f020015;
+        public static final int weathercloud=0x7f020016;
+        public static final int white_cloud=0x7f020017;
+        public static final int whitecloud=0x7f020018;
+        public static final int write=0x7f020019;
     }
     public static final class id {
-        public static final int add_event=0x7f090008;
-        public static final int add_event_row=0x7f090007;
-        public static final int add_event_user_activity=0x7f09000e;
-        public static final int add_event_user_mood=0x7f090010;
-        public static final int add_event_user_thought=0x7f09000f;
-        public static final int anim_view=0x7f090001;
-        public static final int body_count=0x7f09000a;
-        public static final int destroy=0x7f090003;
-        public static final int destroyer=0x7f090002;
-        public static final int fragment_container=0x7f09000d;
-        public static final int game_view=0x7f090000;
-        public static final int graph=0x7f090004;
-        public static final int high_scores=0x7f09000b;
-        public static final int menu_daily_mood=0x7f090012;
-        public static final int menu_destroyer_game=0x7f090011;
-        public static final int menu_settings=0x7f090013;
-        public static final int next=0x7f090006;
-        public static final int previous=0x7f090005;
-        public static final int scientist=0x7f090009;
-        public static final int scoring=0x7f09000c;
+        public static final int activity=0x7f080012;
+        public static final int activity_title=0x7f080011;
+        public static final int add_event_user_activity=0x7f080015;
+        public static final int add_event_user_mood=0x7f080017;
+        public static final int add_event_user_thought=0x7f080016;
+        public static final int anim_view=0x7f080001;
+        public static final int cannon=0x7f080004;
+        public static final int destroy=0x7f080003;
+        public static final int destroyer=0x7f080002;
+        public static final int destroyer_stats_high_score=0x7f08001b;
+        public static final int destroyer_stats_more_scores=0x7f08001c;
+        public static final int destroyer_stats_negatives_destroyed=0x7f08001a;
+        public static final int destroyer_stats_personal_points=0x7f080019;
+        public static final int destroyer_stats_play_game=0x7f080018;
+        public static final int fragment_container=0x7f080009;
+        public static final int friend=0x7f08000f;
+        public static final int friend_space=0x7f080010;
+        public static final int game_view=0x7f080000;
+        public static final int graph=0x7f080005;
+        public static final int home_fragment_container=0x7f080008;
+        public static final int lawyer=0x7f08000d;
+        public static final int lawyer_space=0x7f08000e;
+        public static final int menu_add_event=0x7f08001e;
+        public static final int menu_daily_mood=0x7f08001f;
+        public static final int menu_settings=0x7f080020;
+        public static final int next=0x7f080007;
+        public static final int previous=0x7f080006;
+        public static final int question=0x7f08000a;
+        public static final int scale_it=0x7f08000c;
+        public static final int setup_username=0x7f08001d;
+        public static final int thought=0x7f080014;
+        public static final int thought_title=0x7f080013;
+        public static final int thoughts=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_destroyer=0x7f030000;
-        public static final int activity_graph=0x7f030001;
-        public static final int activity_home=0x7f030002;
-        public static final int activity_main=0x7f030003;
-        public static final int fragment_add_event=0x7f030004;
+        public static final int activity_destroyer_shooter=0x7f030001;
+        public static final int activity_graph=0x7f030002;
+        public static final int activity_home=0x7f030003;
+        public static final int activity_main=0x7f030004;
+        public static final int activity_scale=0x7f030005;
+        public static final int dialog_help=0x7f030006;
+        public static final int dialog_mood=0x7f030007;
+        public static final int fragment_add_event=0x7f030008;
+        public static final int fragment_destroyer_stats=0x7f030009;
+        public static final int fragment_welcome=0x7f03000a;
     }
     public static final class menu {
-        public static final int activity_main=0x7f080000;
-        public static final int activity_set_up=0x7f080001;
+        public static final int activity_main=0x7f070000;
     }
     public static final class raw {
         public static final int bird=0x7f040000;
         public static final int thunder=0x7f040001;
     }
     public static final class string {
-        public static final int activity_daily=0x7f05002e;
-        public static final int activity_education=0x7f050022;
-        public static final int activity_mind=0x7f05002a;
-        public static final int activity_recreation=0x7f050026;
-        public static final int activity_relationship=0x7f050019;
-        public static final int add=0x7f050044;
-        public static final int add_event_fragment_title=0x7f05001a;
-        public static final int add_event_toast_created_event=0x7f05001b;
-        public static final int add_event_user_activity_hint=0x7f05001c;
-        public static final int add_event_user_thought_hint=0x7f05001d;
+        public static final int activity=0x7f05001e;
+        public static final int add_event_fragment_title=0x7f050014;
+        public static final int add_event_toast_created_event=0x7f050015;
+        public static final int add_event_user_activity_hint=0x7f050016;
+        public static final int add_event_user_thought_hint=0x7f050017;
         public static final int app_name=0x7f050000;
-        public static final int body_count=0x7f050048;
-        public static final int complete=0x7f050041;
-        public static final int contact1=0x7f05003c;
-        public static final int contact2=0x7f05003d;
-        public static final int contact3=0x7f05003e;
-        public static final int contact4=0x7f05003f;
-        public static final int contact5=0x7f050040;
-        public static final int continuing=0x7f050014;
-        public static final int daily=0x7f05000e;
-        public static final int daily_desc=0x7f050013;
-        public static final int destroy=0x7f050046;
-        public static final int destroyer=0x7f050045;
-        public static final int education=0x7f05000b;
-        public static final int education_desc=0x7f050010;
-        public static final int evening=0x7f05003b;
-        public static final int hello_world=0x7f050004;
-        public static final int high_scores=0x7f050047;
-        public static final int image_sad_content_description=0x7f05001f;
-        public static final int image_smiley_content_description=0x7f05001e;
-        public static final int instruction_time=0x7f050032;
-        public static final int instructions_contacts=0x7f050039;
-        public static final int life_areas=0x7f050016;
-        public static final int life_areas_daily=0x7f05002d;
-        public static final int life_areas_education=0x7f050020;
-        public static final int life_areas_mind=0x7f050029;
-        public static final int life_areas_recreation=0x7f050025;
-        public static final int life_areas_relationship=0x7f050017;
-        public static final int menu_daily_mood=0x7f05004a;
-        public static final int menu_destroyer_game=0x7f050003;
-        public static final int menu_settings=0x7f050002;
-        public static final int mind=0x7f05000d;
-        public static final int mind_desc=0x7f050012;
-        public static final int morning=0x7f05003a;
-        public static final int next=0x7f05004b;
-        public static final int next2=0x7f050007;
-        public static final int pic_desc=0x7f050042;
-        public static final int previous=0x7f05004c;
-        public static final int recreation=0x7f05000c;
-        public static final int recreation_desc=0x7f050011;
-        public static final int relationship=0x7f05000a;
-        public static final int relationship_activities=0x7f050043;
-        public static final int relationship_desc=0x7f05000f;
-        public static final int scientist=0x7f05004d;
-        public static final int score=0x7f050049;
-        public static final int set_time=0x7f050035;
-        public static final int set_time_evening=0x7f050036;
-        public static final int step1=0x7f050006;
-        public static final int step2=0x7f050009;
-        public static final int title_activity_calendar_start=0x7f050034;
-        public static final int title_activity_contacts=0x7f050038;
-        public static final int title_activity_daily=0x7f05002c;
-        public static final int title_activity_education=0x7f050021;
-        public static final int title_activity_mind=0x7f050028;
-        public static final int title_activity_notification_morning=0x7f050033;
-        public static final int title_activity_recreation=0x7f050024;
-        public static final int title_activity_reminder=0x7f050037;
-        public static final int title_activity_set_up=0x7f050030;
-        public static final int title_activity_step1=0x7f050005;
-        public static final int title_activity_step2=0x7f050008;
-        public static final int title_activity_step3=0x7f050015;
-        public static final int title_activity_time=0x7f050031;
-        public static final int value_daily=0x7f05002f;
-        public static final int value_education=0x7f050023;
-        public static final int value_mind=0x7f05002b;
-        public static final int value_recreation=0x7f050027;
-        public static final int value_relationship=0x7f050018;
-        public static final int welcome_fragment_message=0x7f050001;
+        public static final int destroy=0x7f05001b;
+        public static final int destroyer=0x7f05001a;
+        public static final int destroyer_stas_personal_points=0x7f050010;
+        public static final int destroyer_stats_high_score=0x7f050011;
+        public static final int destroyer_stats_more_scores=0x7f050012;
+        public static final int destroyer_stats_negatives_destroyed=0x7f05000f;
+        public static final int destroyer_stats_play_game=0x7f05000e;
+        public static final int destroyer_stats_title=0x7f05000d;
+        public static final int friend=0x7f050023;
+        public static final int high_scores_title=0x7f050013;
+        public static final int image_sad_content_description=0x7f050019;
+        public static final int image_smiley_content_description=0x7f050018;
+        public static final int lawyer=0x7f050022;
+        public static final int menu_add_event=0x7f05000a;
+        public static final int menu_daily_mood=0x7f05000b;
+        public static final int menu_settings=0x7f05000c;
+        public static final int next=0x7f05001c;
+        public static final int previous=0x7f05001d;
+        public static final int scale_it=0x7f050021;
+        public static final int skip=0x7f050024;
+        public static final int thought=0x7f05001f;
+        public static final int thought_it=0x7f050020;
+        public static final int welcome_text_aim_app=0x7f050003;
+        public static final int welcome_text_build_skills=0x7f050008;
+        public static final int welcome_text_know_activities=0x7f050006;
+        public static final int welcome_text_personal_scientist=0x7f050005;
+        public static final int welcome_text_replace_negative=0x7f050007;
+        public static final int welcome_text_skills_are=0x7f050004;
+        public static final int welcome_text_thank_you=0x7f050002;
+        public static final int welcome_title=0x7f050001;
+        public static final int welcome_username_hint=0x7f050009;
     }
     public static final class style {
         /** 
@@ -173,12 +154,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int Custom=0x7f070002;
-        public static final int TimePicker=0x7f070003;
+        public static final int AppTheme=0x7f060001;
+        public static final int Custom=0x7f060002;
+        public static final int TimePicker=0x7f060003;
     }
 }
