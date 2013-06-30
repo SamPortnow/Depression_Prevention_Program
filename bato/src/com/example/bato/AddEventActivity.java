f0857e2eeeeec5565a07a92b6f7608aafe2360cf
redseiko <redseiko@gmail.com>
2013-06-30 14:10:47 -0600

    Temporary commit for "feeling."

--- bato/src/com/example/bato/AddEventActivity.java
+++ bato/src/com/example/bato/AddEventActivity.java
@@ -1,7 +1,11 @@
 package com.example.bato;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class AddEventActivity extends Activity
 {
@@ -13,5 +17,30 @@ public class AddEventActivity extends Activity
 		setContentView(R.layout.activity_add_event);
 		
 		getFragmentManager().beginTransaction().replace(R.id.fragment_container, new AddEventUserActivityFragment()).commit();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.activity_add_event, menu);
+		
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		if (item.getItemId() == R.id.cancel_task)
+		{
+			Intent intent = new Intent(this, MainActivity.class);
+			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			
+			startActivity(intent);
+		}
+		else
+			return super.onOptionsItemSelected(item);
+		
+		return true;
 	}
 }
--- bato/src/com/example/bato/AddEventUserFeelingFragment.java
+++ bato/src/com/example/bato/AddEventUserFeelingFragment.java
@@ -1,19 +1,24 @@
 package com.example.bato;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 
 public class AddEventUserFeelingFragment extends Fragment
 {	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-	    super.onCreate(savedInstanceState);
+	    super.onCreate(savedInstanceState);	   
 
 	    View view = inflater.inflate(R.layout.fragment_add_event_user_feeling, null);
+	    
+	    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+	    imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
 
 	    return view;
 	}
