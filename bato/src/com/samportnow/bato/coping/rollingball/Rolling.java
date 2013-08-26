38a17c397de5ab7dbeb0cba134d58216624b30a8
SamPortnow <samson91787@gmail.com>
2013-08-26 18:06:43 -0400

    new smoothing for rolling ball

--- bato/src/com/samportnow/bato/coping/rollingball/Rolling.java
+++ bato/src/com/samportnow/bato/coping/rollingball/Rolling.java
@@ -7,18 +7,18 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.util.Log;
 
 
 public class Rolling extends Activity implements SensorEventListener {
 	
 	private SensorManager sensorManager;
 	private MyRenderer MyRenderer;
-//	float xVal[] = new float[2];
-	float xVal;
 	double ax, ay, az;
-	
+	static final float ALPHA = 0.2f;
+	protected float[] accelVals;
 	GLSurfaceView glSurface;
-	
+	Sensor accelerometer; 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -28,30 +28,48 @@ public class Rolling extends Activity implements SensorEventListener {
 		glSurface.setRenderer(MyRenderer);
 		setContentView(glSurface);
         sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+		}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		sensorManager.unregisterListener(this);
+		}
 
 	@Override
 	public void onAccuracyChanged(Sensor arg0, int arg1) {
 		
 	}
-
+	
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType()==Sensor.TYPE_ACCELEROMETER)
-        {
-        	float kFilteringFactor = 0.1f;
+	    if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+	        accelVals = lowPass( event.values, accelVals );
+	    	Log.e("my renderer", "" + MyRenderer.x);
+	    	MyRenderer.x = accelVals[0];
 
-
-        	//acceleration.x,.y,.z is the input from the sensor
-
-        	//result.x,.y,.z is the filtered result
-
-        	//high-pass filter to eliminate gravity
-        	xVal = event.values[0] * kFilteringFactor + xVal * (1.0f - kFilteringFactor);
-        	MyRenderer.x = event.values[0] - xVal;
-        }
+	    // use smoothed accelVals here; see this link for a simple compass example:
+	    // http://www.codingforandroid.com/2011/01/using-orientation-sensors-simple.html
 	}
-	
+
+	/**
+	 * @see http://en.wikipedia.org/wiki/Low-pass_filter#Algorithmic_implementation
+	 * @see http://developer.android.com/reference/android/hardware/Sensor.html#TYPE_ACCELEROMETER
+	 */
+	protected float[] lowPass( float[] input, float[] output ) {
+	    if ( output == null ) return input;
+
+	    for ( int i=0; i<input.length; i++ ) {
+	        output[i] = output[i] + ALPHA * (input[i] - output[i]);
+	    }
+	    return output;
+	}
 
 }
