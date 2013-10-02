72991d4cbd01df1791fed0381d752225dca88eb9
redseiko <redseiko@gmail.com>
2013-10-02 16:26:15 -0600

    Added date support for High Scores in CannonSummary.

--- bato/src/com/samportnow/bato/CannonSummaryFragment.java
+++ bato/src/com/samportnow/bato/CannonSummaryFragment.java
@@ -1,8 +1,10 @@
 package com.samportnow.bato;
 
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,6 +23,8 @@ import com.samportnow.bato.destroyer.DestroyerGame;
 
 public class CannonSummaryFragment extends Fragment
 {
+	private List<HighScoreDao> mHighScores = null;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
@@ -49,22 +54,21 @@ public class CannonSummaryFragment extends Fragment
 					startActivity(intent);
 				}
 			});
+			
+			BatoDataSource dataSource = new BatoDataSource(getActivity()).open();
+			
+			mHighScores = dataSource.getTopHighScores(10);
+			dataSource.close();
 
-			final String[] highScores = getHighScores();
-			((TextView) view.findViewById(R.id.cannon_summary_high_score)).setText(highScores[0]);
+			String highScore = mHighScores.isEmpty() ? "0" : String.valueOf(mHighScores.get(0).getScore());
+			((TextView) view.findViewById(R.id.cannon_summary_high_score)).setText(highScore);
 
 			view.findViewById(R.id.cannon_summary_see_more).setOnClickListener(new View.OnClickListener()
 			{
 				@Override
 				public void onClick(View v)
 				{
-					AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-
-					builder.setTitle(R.string.cannon_summary_high_scores_dialog_title);
-					builder.setItems(highScores, null);
-					builder.setPositiveButton(android.R.string.ok, null);
-
-					builder.create().show();
+					createHighScoresDialog().show();
 				}
 			});
 		}
@@ -73,6 +77,50 @@ public class CannonSummaryFragment extends Fragment
 			view.findViewById(R.id.cannon_summary_locked_container).setVisibility(View.VISIBLE);
 			view.findViewById(R.id.cannon_summary_container).setVisibility(View.GONE);
 		}
+	}
+	
+	private Dialog createHighScoresDialog()
+	{
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		
+		BaseAdapter adapter = new BaseAdapter()
+		{
+			public View getView(int position, View convertView, ViewGroup parent)
+			{
+				if (convertView == null)
+					convertView = LayoutInflater.from(getActivity()).inflate(R.layout.listitem_high_score, parent, false);
+				
+				HighScoreDao highScore = mHighScores.get(position);
+				
+				GregorianCalendar calendar = new GregorianCalendar();
+				calendar.setTimeInMillis(highScore.getCreated());
+				
+				((TextView) convertView.findViewById(R.id.high_score_value)).setText(String.valueOf(highScore.getScore()));
+				((TextView) convertView.findViewById(R.id.high_score_timestamp)).setText(calendar.getTime().toString());
+				
+				return convertView;
+			}
+			
+			public long getItemId(int position)
+			{
+				return mHighScores.get(position).getId();
+			}
+			
+			public Object getItem(int position)
+			{
+				return mHighScores.get(position);
+			}
+			
+			public int getCount()
+			{
+				return mHighScores.size();
+			}
+		};
+		
+		builder.setPositiveButton(android.R.string.ok, null);
+		builder.setAdapter(adapter, null);
+		
+		return builder.create();
 	}
 
 	private boolean isUnlocked()
@@ -83,25 +131,5 @@ public class CannonSummaryFragment extends Fragment
 		dataSource.close();
 		
 		return (challenges.size() > 0);
-	}
-
-	private String[] getHighScores()
-	{
-		BatoDataSource dataSource = new BatoDataSource(getActivity()).open();
-		
-		List<HighScoreDao> highScores = dataSource.getTopHighScores(10);
-		dataSource.close();
-		
-		// TODO: save the List and use that in the Dialog.
-		
-		if (highScores.isEmpty())
-			return new String[] { "0" };
-		
-		String[] values = new String[highScores.size()];
-		
-		for (int i = 0; i < highScores.size(); i++)
-			values[i] = String.valueOf(highScores.get(i).getScore());
-
-		return values;
 	}
 }
